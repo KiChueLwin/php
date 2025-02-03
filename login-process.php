f2a8a4023c0237b8383b4a93004a05a2dff48f79
Ki Chue Lwin <121808287+KiChueLwin@users.noreply.github.com>
2025-02-03 16:40:54 +0630

    Update login-process.php

--- login-process.php
+++ login-process.php
@@ -10,7 +10,13 @@
 
     <?php
 
+
+
+
+
     if(isset($_POST['login'])){
+
+        ///////////////////////////////////////////////////////////////
 
         $email=$_POST['email'];
         $pwd=$_POST['pwd'];
@@ -44,9 +50,44 @@
         echo "<script>window.location.herf='login.php'</script>";
     }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    ?>
+  ---------------->  Alternative way  to login <---------
+
+        $email=$_POST['email'];
+        $pwd=$_POST['pwd'];
+
+        // testing for data carrying
+        // echo "$email";
+        // echo "$pwd";
+
+        $sql = "select * from customer where email='$email'";
+
+        $result = mysqli_query($connect,$sql);
+        $num_rows = mysqli_num_rows($result);
+
+        if($num_rows==1)
+        {
+            $record=mysqli_fetch_assoc($result);
+            $hashed_pw= $record['Password'];
+
+            if(password_verify($pwd,$hashed_pw))
+            {
+                echo "Login successful!";  
+            }
+            else
+            {
+                echo "Invalid Password! Please try again!";
+            }
+        }
+        else
+        {
+            echo "Your email is invalid!";
+        }
+
+
+      ?>
 
 
 </body>
